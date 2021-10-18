810d30a83dc460873a3c2518ede8019db5277b89
nicow222 <angelonicolasw@gmail.com>
2021-10-18 10:02:06 -0300

    Pinchos

--- juego/personaje/Saltarina.gd
+++ juego/personaje/Saltarina.gd
@@ -18,7 +18,7 @@ func _physics_process(delta):
 	caer()
 	saltar()
 	colision_techo()
-	respawn()
+	caida_al_vacio()
 		
 	move_and_slide(movimiento, Vector2.UP)
 	
@@ -54,6 +54,11 @@ func colision_techo():
 	if is_on_ceiling():
 		movimiento.y = fuerza_rebote
 
-func respawn():
+func caida_al_vacio():
 	if position.y > camara.limit_bottom:
-		get_tree().reload_current_scene()
+		respawn()
+
+func respawn():
+	get_tree().reload_current_scene()
+		
+
